666b9ed5cc0c119e4597ab62c00e42efec5c7006
Sean Lee <leejoneshane@gmail.com>
2016-04-12 14:22:11 +0800

    add first.sh for the container first launch

---
+++ first.sh
@@ -0,0 +1,3 @@
+drush -y en locale translation views date calendar ctools views services libraries l10n_update
+drush -y en simsauth sims_views sims_field gapps db2health adsync gevent thumbnail_link
+rm -rf /usr/sbin/first.sh
