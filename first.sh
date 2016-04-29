1a564e11cc777943b1cb41182ed42c55bbbb04f3
Sean Lee <leejoneshane@gmail.com>
2016-04-30 01:36:00 +0800

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 drush -y en locale translation views date calendar ctools views services libraries l10n_update
 drush -y en simsauth sims_views sims_field gapps db2health adsync gevent thumbnail_link
 rm -rf /usr/sbin/first.sh
