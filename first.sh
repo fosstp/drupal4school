c33901ec3ac2947a7fc5e7c87dc92d25bd8a1bff
Sean Lee <leejoneshane@gmail.com>
2016-04-30 01:34:14 +0800

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,3 +1,6 @@
+drush language-add zh-hant
+drush language-enable zh-hant
+drush language-default zh-hant
 drush -y en locale translation views date calendar ctools views services libraries l10n_update
 drush -y en simsauth sims_views sims_field gapps db2health adsync gevent thumbnail_link
 rm -rf /usr/sbin/first.sh
