080bb1efccadf672a988c960877b2ec6594b186b
Sean Lee <leejoneshane@gmail.com>
2016-09-14 08:30:23 +0800

    "set env" run once.

--- run-httpd.sh
+++ run-httpd.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 rm -f /var/run/apache2/apache2.pid
+FIRST_STARTUP_DONE="/var/log/docker-drupal-first-startup-done"
 
-echo "export $(env | grep DATABASE_PASSWORD)" >> /etc/apache2/envvars
+if [ ! -e "$FIRST_STARTUP_DONE" ]; then
+  echo "export $(env | grep DATABASE_PASSWORD)" >> /etc/apache2/envvars
+  touch ${FIRST_STARTUP_DONE}
+fi
+
 source /etc/apache2/envvars
 exec apache2 -DFOREGROUND
