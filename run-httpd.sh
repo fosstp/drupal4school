9c8e6696189adc87700a5bf9ed0e85945a349455
leejoneshane <leejoneshane@gmail.com>
2019-12-24 11:04:54 +0800

    update docker entrypoint

--- run-httpd.sh
+++ run-httpd.sh
@@ -3,8 +3,6 @@ set -e
 
 if [ ! -f "/var/run/apache2/apache2.pid" ]; then
     #php /var/www/html/core/scripts/drupal install drupal4school
-    chown -R www-data:www-data /var/www/html/modules
-    chown -R www-data:www-data /var/www/html/themes
     chown -R www-data:www-data /var/www/html/sites
     chmod 744 /var/www/html/sites/default/files
     exec apache2-foreground
