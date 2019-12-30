2ad307a60ca970c4a8a4e3f84c6aafc41632d73d
leejoneshane <leejoneshane@gmail.com>
2019-12-30 15:10:02 +0800

    fix bug

--- modules/tpedu/src/Controller/tpeduController.php
+++ modules/tpedu/src/Controller/tpeduController.php
@@ -2,11 +2,12 @@
 
 namespace Drupal\tpedu\Controller;
 
+use Drupal\Core\Url;
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpFoundation\Session;
-use Symfony\Component\HttpFoundation\Request;
 use Drupal\Component\Utility;
 use Drupal\user\Entity\User;
+use Symfony\Component\HttpFoundation\Session;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
