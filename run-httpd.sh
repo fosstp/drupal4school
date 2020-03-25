6b3917ac67ec94066ea3ec2090eb378941b0202c
leejoneshane <leejoneshane@gmail.com>
2020-03-25 14:15:42 +0800

    update Dockerfile

--- run-httpd.sh
+++ run-httpd.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 set -e
+if [ ! -d "/var/www/html/vendor/google" ]; then
+    composer require google/apiclient:"^2.0"
+fi
+if [ ! -d "/var/www/html/vendor/drush" ]; then
+    composer require drush/drush
+    ln -s /var/www/html/vendor/bin/drush /usr/local/bin/drush
+    chmod 744 /usr/local/bin/drush
+fi
 
 if [ ! -f "/var/run/apache2/apache2.pid" ]; then
     #php /var/www/html/core/scripts/drupal install drupal4school
