19c09768d56a392676a2c540420fb159cebab4bc
leejoneshane <leejoneshane@gmail.com>
2020-05-11 09:59:18 +0800

    update

--- modules/tpedu/src/Plugin/Field/FieldWidget/ClassesDefaultWidget.php
+++ modules/tpedu/src/Plugin/Field/FieldWidget/ClassesDefaultWidget.php
@@ -3,6 +3,7 @@
 namespace Drupal\tpedu\Plugin\Field\FieldWidget;
 
 use Drupal\Component\Utility\Html;
+use Drupal\Component\Utility\NestedArray;
 use Drupal\Core\Field\FieldFilteredMarkup;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\WidgetBase;
@@ -90,24 +91,11 @@ class ClassesDefaultWidget extends WidgetBase
         $path = array_merge($form['#parents'], [$field_name]);
         $key_exists = null;
         $values = NestedArray::getValue($form_state->getValues(), $path, $key_exists);
-
+        if (!is_array($values)) {
+            $values = array($values);
+        }
         if ($key_exists) {
-            // Account for drag-and-drop reordering if needed.
-            if (!$this->handlesMultipleValues()) {
-                // The original delta, before drag-and-drop reordering, is needed to
-                // route errors to the correct form element.
-                foreach ($values as $delta => &$value) {
-                    $value['_original_delta'] = $delta;
-                }
-                usort($values, function ($a, $b) {
-                    return SortArray::sortByKeyInt($a, $b, '_weight');
-                });
-            }
-
-            // Let the widget massage the submitted values.
             $values = $this->massageFormValues($values, $form, $form_state);
-
-            // Assign the values and remove the empty ones.
             $items->setValue($values);
             $items->filterEmptyItems();
 
@@ -121,6 +109,30 @@ class ClassesDefaultWidget extends WidgetBase
         }
     }
 
+    public static function validateElement(array $element, FormStateInterface $form_state)
+    {
+        if ($element['#required'] && $element['#value'] == '_none') {
+            $form_state->setError($element, t('@name field is required.', ['@name' => $element['#title']]));
+        }
+
+        if (is_array($element['#value'])) {
+            $values = array_values($element['#value']);
+        } else {
+            $values = [$element['#value']];
+        }
+
+        $index = array_search('_none', $values, true);
+        if ($index !== false) {
+            unset($values[$index]);
+        }
+
+        $items = [];
+        foreach ($values as $value) {
+            $items[] = [$element['#key_column'] => $value];
+        }
+        $form_state->setValueForElement($element, $items);
+    }
+
     public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state)
     {
         $element['#delta'] = $delta;
@@ -130,6 +142,7 @@ class ClassesDefaultWidget extends WidgetBase
         $element['#options'] = $options;
         $this->required = $this->fieldDefinition->isRequired();
         $this->multiple = $this->fieldDefinition->getFieldStorageDefinition()->isMultiple();
+        $this->has_value = isset($items[0]->class_id);
         if (!$this->multiple && !$this->required) {
             $element['#empty_option'] = '--';
             $element['#empty_value'] = '';
@@ -140,6 +153,7 @@ class ClassesDefaultWidget extends WidgetBase
         if ($this->multiple) {
             $element['#type'] = 'checkboxes';
             $this->display_inline($element);
+            $element['#default_value'] = $this->getSelectedOptions($items);
         } else {
             $element['#type'] = 'select';
             $value = isset($items[$delta]->class_id) ? $items[$delta]->class_id : '';
@@ -184,6 +198,21 @@ class ClassesDefaultWidget extends WidgetBase
         }
 
         return $options;
+    }
+
+    protected function getSelectedOptions(FieldItemListInterface $items)
+    {
+        $flat_options = OptGroup::flattenOptions($this->getOptions());
+
+        $selected_options = [];
+        foreach ($items as $item) {
+            $value = $item->class_id;
+            if (isset($flat_options[$value])) {
+                $selected_options[] = $value;
+            }
+        }
+
+        return $selected_options;
     }
 
     public function display_inline(array &$element)
