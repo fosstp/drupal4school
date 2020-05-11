95e96fe504f6387aa921a230cd646bc757eb1c11
leejoneshane <leejoneshane@gmail.com>
2020-05-11 13:09:09 +0800

    修正權限

--- run-httpd.sh
+++ run-httpd.sh
@@ -3,8 +3,9 @@ set -e
 
 if [ ! -f "/var/run/apache2/apache2.pid" ]; then
     #php /var/www/html/core/scripts/drupal install drupal4school
-    chown -R www-data:www-data /var/www/html/sites
-    chmod 775 /var/www/html/sites/default/files
+    chown -R root:www-data /var/www/html
+    chmod -R 750 /var/www/html
+    chmod -R 770 /var/www/html/sites/default/files
     exec apache2-foreground
 fi
 
