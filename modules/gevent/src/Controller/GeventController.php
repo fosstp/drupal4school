b17314f7b09d9fd25bc66f70b5f0e34b06eae426
leejoneshane <leejoneshane@gmail.com>
2020-09-20 11:34:29 +0800

    patch bugs

--- modules/gevent/src/Controller/GeventController.php
+++ modules/gevent/src/Controller/GeventController.php
@@ -9,12 +9,14 @@ class GeventController extends ControllerBase
 {
     public function todolist(Request $request)
     {
-        $form = [];
-        $form['#attached']['library'][] = 'gevent/gevent_print';
+        $build = [];
+        $build['#attached']['library'][] = 'gevent/gevent_print';
+        initGoogleCalendar();
         date_default_timezone_set('Asia/Taipei');
         $mydate = gevent_current_seme();
         $title = '第'.$mydate['stryear'].'學年第'.$mydate['seme'].'學期行事曆';
-        $events = gs_listEvents();
+        $calid = \Drupal::config('gevent.settings')->get('calendar_id');
+        $events = gs_listEvents($calid);
         $build['todolist'] = [
             '#theme' => 'gevent_todolist',
             '#title' => $title,
